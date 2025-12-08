77e664a7ebd2d0d2d853b55e81a59817e3b3cd8e
이진우 <leejinwoo1126@gmail.com>
2025-12-08 21:57:28 +0900

    git init

---
+++ CSharpBasicsTest/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using Shouldly;
+using Xunit.Abstractions;
+
+namespace CSharpBasicsTest;
+
+public class UnitTest1
+{
+    private readonly ITestOutputHelper _output;
+
+    public UnitTest1(ITestOutputHelper output)
+    {
+        _output = output;    
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var num1 = 1;
+        var num2 = 2;
+
+        int sum = num1 + num2;
+
+        sum.ShouldBe(3);    
+    }
+}
